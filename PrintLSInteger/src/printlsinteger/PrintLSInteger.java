1fae1945b6747cc3cb8a49a9156badf6071b1200
apollo1986 <teyyubeyyuboglu@gmail.com>
2017-09-14 12:00:05 +0400

    create first program

---
+++ PrintLSInteger/src/printlsinteger/PrintLSInteger.java
@@ -0,0 +1,24 @@
+package printlsinteger;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author teyyub Sep 14, 2017
+ */
+public class PrintLSInteger {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number, i = 0, max,min;
+        while (i < 5) {
+            number = scan.nextInt();
+            
+            i++;
+        }
+    }
+
+}
