cf8b357771a8461d60530e9b0f227e89f05ca142
apollo1986 <teyyubeyyuboglu@gmail.com>
2017-09-17 15:45:08 +0400

    add find maximum number

---
+++ PrintLSInteger/src/printlsinteger/NewClass.java
@@ -0,0 +1,11 @@
+ 
+
+package printlsinteger;
+
+/**
+ *
+ * @author teyyub Sep 17, 2017 1:30:19 PM
+ */
+public class NewClass {
+
+}
--- PrintLSInteger/src/printlsinteger/PrintLSInteger.java
+++ PrintLSInteger/src/printlsinteger/PrintLSInteger.java
@@ -12,16 +12,25 @@ public class PrintLSInteger {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        
+   
+
         Scanner scan = new Scanner(System.in);
-        int number, i = 0, max,min=0;
-        while (i < 5) {
-            number = scan.nextInt();
+        int number, i = 0, max=0,min=0;
+        min = scan.nextInt();
+        max = min;
+        while (i < 4) {
+            number = scan.nextInt();             
             if(number<min){
-                min = number;
+                 min = number;
             } 
+             if(number>max){
+               max = number;
+            }
             i++;
         }
-        System.out.println("min="+min); 
-    }
+        System.out.println("min="+min+" max= "+max); 
+}
 
 }
